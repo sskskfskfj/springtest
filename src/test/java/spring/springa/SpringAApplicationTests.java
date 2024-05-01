20ecf9e84811e1bf7c3eebe5a14774d071ca5fbb
sskskfskfj <dongkyun7717@gmail.com>
2024-05-01 23:30:59 +0900

    Changes

---
+++ src/test/java/spring/springa/SpringAApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.springa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
