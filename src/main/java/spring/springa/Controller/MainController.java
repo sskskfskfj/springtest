3c7f05a8ad237143b1d024951e2389d21f8aa7a3
sskskfskfj <dongkyun7717@gmail.com>
2024-05-02 21:01:48 +0900

    05.02

--- src/main/java/spring/springa/Controller/MainController.java
+++ src/main/java/spring/springa/Controller/MainController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
@@ -11,11 +12,23 @@ public class MainController {
     public String hello(Model model){
         model.addAttribute("data", "hello");
         return "hello";
-
     }
     @GetMapping("/hello-mvc")
     public String helloMvc(@RequestParam(value = "name") String name, Model model){
         model.addAttribute("name", name);
-        return "hello";
+        return "hello-template";
+    }
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello" + name;
+
+    }
+    @GetMapping("hello-api")
+    @ResponseBody
+
+    public static class Hello{
+        private String name;
+
     }
 }
